b3f0bf63186a730388cab84b4a376fe7d42b3cc9
Jasmine Kapadia <jk4660@barnard.edu>
2023-01-09 12:04:51 -0500

    basic bio done

---
+++ components/Home.vue
@@ -0,0 +1,9 @@
+<template>
+    <div>
+        <h1>welcome!</h1>
+		<p> Jasmine Kapadia (she/her) is 18, Asian-American, and based in NYC. She studies Computational Liguistics at Barnard College of Columbia University. </p>
+        <p> Find her on instagram: </p>
+		<a href="https://www.instagram.com/jazzymoons/" target="_blank">
+			here's the link </a>
+    </div>
+</template>
